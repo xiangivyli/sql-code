136a00fe99da398c8eca2986b4c590588465a0fd
Ivy Li <xiangivyli@gmail.com>
2023-05-15 15:05:00 +0100

    Update MetaData.sql

--- MetaData.sql
+++ MetaData.sql
@@ -1,23 +1,32 @@
 
+/****** INFORMATION_SCHEMA.TABLES/COLUMNS/CONSTRAINTS ******/
 -- Databases store metadata in tables
 
--- list table schemas, included tables and table tppes
+-- 1.list table schemas, included tables and table tppes
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES;
 
--- list column names and position, types, and other detailed info
+-- 2.list column names and position, types, and other detailed info
 SELECT *
 FROM INFORMATION_SCHEMA.COLUMNS;
 
--- list constraints defined on tables
+-- 3.list constraints defined on tables
 SELECT *
 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS;
 
--- list check constraints defined on tables
+-- 4.list check constraints defined on tables
 SELECT *
 FROM INFORMATION_SCHEMA.CHECK_CONSTRAINTS;
 
 
--- list details about the foreign key constraints and their relationships to primary key constraints
+-- 5.list details about the foreign key constraints and their relationships to primary key constraints
 SELECT *
 FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS;
+
+
+/****** EXPLORE ONE TABLE WITH INFORMATION_SCHEMA ******/
+
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'CovidDeathsCountry'
+ AND TABLE_SCHEMA = 'dbo';
