395288db58d3d7439ed05236c7034c47f00c4de2
Ivy Li <xiangivyli@gmail.com>
2023-05-15 15:45:25 +0100

    Create Split_Data_Into_Tables.sql

---
+++ Split_Data_Into_Tables.sql
@@ -0,0 +1,41 @@
+
+
+
+/*Step 1, Create 4 empty tables*/
+
+-- Create a table for the professors entity type
+CREATE TABLE professors (
+ firstname text,
+ lastname text
+);
+
+-- Add the university_shortname column
+ALTER TABLE professors
+ADD university_shortname text;
+
+-- Change the column name
+EXEC sp_rename 'professors.lastname', 'familyname', 'COLUMN';
+
+-- Create a table for the universities entity type
+CREATE TABLE universities (
+    university_shortname text,
+    university text,
+    university_city text
+);
+
+/*Step 2, Migrate data from old table*/
+
+-- Insert selected data (distinct)
+INSERT INTO professors
+SELECT DISTINCT firstname,
+       familyname,
+FROM university_professors;
+
+
+INSERT INTO universities
+SELECT DISTINCT university_shortname,
+       university,
+	   university
+FROM university_professors;
+
+
